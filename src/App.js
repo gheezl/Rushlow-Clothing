658ea555bb6c30d15daee5f5baa4b071e8cadedc
gheezl <gheezl25@gmail.com>
2020-07-03 17:55:03 -0400

    okay

--- src/App.js
+++ src/App.js
@@ -19,14 +19,29 @@ class App extends Component {
     }
   }
 
+
   unsubscribeFromAuth = null
 
+
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
-      createUserProfileDocument(user);
-      this.setState({ currentUser: user })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapshot => {
+          this.setState({
+            currentUser: {
+              id: snapshot.id
+            }
+          }, () => console.log(this.state))
+        })
+      }
+      else {
+        this.setState({ currentUser: userAuth }, () => console.log(userAuth))
+      }
     })
   }
+
 
   componentWillUnmount() {
     this.unsubscribeFromAuth();
