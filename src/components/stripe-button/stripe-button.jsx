6e259a20c9dae015408a20b0a0190391e7724e8e
gheezl <gheezl25@gmail.com>
2020-07-13 12:42:42 -0400

    I added strip integration

---
+++ src/components/stripe-button/stripe-button.jsx
@@ -0,0 +1,37 @@
+import React, { Fragment } from "react"
+import { connect } from "react-redux"
+import StripeCheckout from "react-stripe-checkout"
+import { createStructuredSelector } from "reselect";
+import cartTotal from "../../redux/cart/selectors/cart-total.selector.js"
+
+const onToken = token => {
+    console.log(token);
+    alert("Payment Successful")
+}
+
+const StripeCheckoutButton = ({ price }) => {
+    const PriceForStripe = price * 100
+    const PublishableKey = "pk_test_51H4TcXKK0gugLsixM4c4qLYjnQwBfNCWsAR00gvt5cbVlKlq2MFfVaMKL5wGhK2r6FfLLF1KDt0HxwgArL8XPB4200qdnkBYD3"
+    return (
+        <Fragment>
+            <StripeCheckout
+                label="Pay Now"
+                name="CRWN Clothing"
+                billingAddress
+                shippingAddress
+                image="https://sendeyo.com/up/d/f3eb2117da"
+                description={`Your total is ${price} USD`}
+                amount={PriceForStripe}
+                panelLabel="Pay Now"
+                token={onToken}
+                stripeKey={PublishableKey}
+            />
+        </Fragment>
+    )
+}
+
+const mapStateToProps = createStructuredSelector({
+    price: cartTotal
+})
+
+export default connect(mapStateToProps)(StripeCheckoutButton);
--- src/pages/checkout/checkout.jsx
+++ src/pages/checkout/checkout.jsx
@@ -4,6 +4,7 @@ import { createStructuredSelector } from "reselect"
 
 import selectCartItems from "../../redux/cart/selectors/cart-items.selector.js"
 import cartTotal from "../../redux/cart/selectors/cart-total.selector.js"
+import StripeCheckoutButton from "../../components/stripe-button/stripe-button.jsx"
 
 import "./checkout.scss"
 import CheckOutItem from "../../components/checkout-item/checkout-item.jsx";
@@ -27,6 +28,18 @@ const CheckOut = ({ cartItems, total }) => {
                 </div>
                 <div className="total">
                     <span>TOTAL: ${total}</span>
+                </div>
+                <div className="disclaimer" >
+                    DISCLAIMER
+                </div>
+                <div className="test-warning">
+                    *Please use the following test credit card for your payments, thank you!*
+                </div>
+                <div className="test-card">
+                    4242 4242 4242 4242 - Expires: 01/25 - CVV: 789
+                </div>
+                <div className="stripe">
+                    <StripeCheckoutButton />
                 </div>
             </div>
         </Fragment>
