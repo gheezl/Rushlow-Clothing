c5fdcb2fdb151b703b4060b4f56613c88367190d
gheezl <gheezl25@gmail.com>
2020-07-22 17:39:58 -0400

    I am trying this again

--- client/src/components/stripe-button/stripe-button.jsx
+++ client/src/components/stripe-button/stripe-button.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment, Component } from "react"
+import React, { Fragment } from "react"
 import { connect } from "react-redux"
 import { createStructuredSelector } from "reselect";
 import axios from "axios"
