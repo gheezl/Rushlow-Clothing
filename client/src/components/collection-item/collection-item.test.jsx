eb0ab79ac2ec3d74213dcfb5673d600115c1fd5f
gheezl <gheezl25@gmail.com>
2020-07-29 20:43:36 -0400

    Final commit of the day

--- client/src/components/collection-item/collection-item.test.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { shallow } from "enzyme";
-import "./collection-item.jsx"
-
-it("Expect to render Collection Item component", () => {
-    expect(shallow(<CollectionItem />)).toMatchSnapshot()
-})
--- client/src/components/sign-up/sign-up.test.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { shallow } from "enzyme";
-import "./sign-up.jsx"
-
-it("Expect to render Collection Item component", () => {
-    expect(shallow(<SignUp />)).toMatchSnapshot()
-})
